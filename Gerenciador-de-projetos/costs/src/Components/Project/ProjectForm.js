f7433a733ac8756f11505801fa29e5898835b1ab
Victor costa <costavictor771@gmail.com>
2022-12-21 17:29:49 -0300

    Api Form

--- Gerenciador-de-projetos/costs/src/Components/Project/ProjectForm.js
+++ Gerenciador-de-projetos/costs/src/Components/Project/ProjectForm.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
@@ -5,6 +7,24 @@ import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
 function ProjectForm({ btnText }){
+    const[categories, setCategories] = useState([])
+
+    useEffect(() => {
+
+        fetch("http://localhost:5000/categories", {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+        }).then((resp) => resp.json())
+        .then((data) => {
+            setCategories(data)
+        })
+        .catch(err =>console.log(err))
+    
+    }, [])
+
+        
     return(
         
         <form className={styles.form}>
@@ -24,7 +44,7 @@ function ProjectForm({ btnText }){
            placeholder="Insira o nome do projeto"
            />
 
-            <Select name="category-id" text="Selecione a categoria"/>
+            <Select name="category-id" text="Selecione a categoria" options={categories}/>
             
            <SubmitButton text={btnText}/>
         </form>
--- Gerenciador-de-projetos/costs/src/Components/form/Select.js
+++ Gerenciador-de-projetos/costs/src/Components/form/Select.js
@@ -5,7 +5,12 @@ function Select ({ text, name, options, handleOnChange, value }){
         <div className={styles.form_control}>
             <label htmlFor="{name}">{text}</label>
             <select name={name} id={name}>
-            <option>Selecione uma opção</option>
+                <option>Selecione uma opção</option>
+                    {options.map((option) => (
+                    <option value={option.id} key={option.id}>
+                    {option.name}
+                </option>
+            ))}
 
             </select>
 
